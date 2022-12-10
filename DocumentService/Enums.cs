8afff9cdb84c4202edf3cc89bee7897683c685f2
Bora KAŞMER <bora@borakasmer.com>
2022-12-10 14:34:05 +0300

    Add Service Layer

---
+++ DocumentService/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace DocumentService
+{
+    public enum DocumentType
+    {
+        Opened = 1, Clicked = 2
+    }
+    public class Enums
+    {
+
+    }
+}
---
+++ DocumentService/ReadService.cs
@@ -0,0 +1,52 @@
+﻿using DAL.Entities;
+using DAL.Entities.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentService
+{
+    public class ReadService : IReadService
+    {
+        DocumentContext _context;
+        public ReadService(DocumentContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> IncrementDocumentOpenedCountAsync(int userID, int attachmentID, int actionTypeID)
+        {
+            var updatedDocument = await _context.DocumentStatistics.FirstOrDefaultAsync(ds => 
+            ds.UserId == userID && ds.AttachmentId == attachmentID);
+            var count = 0;
+            if (updatedDocument != null)
+            {
+                if (actionTypeID == (int)DocumentType.Opened)
+                {
+                    count = updatedDocument.OpenedCount + 1;
+                    updatedDocument.OpenedCount = count;
+                }
+                else if (actionTypeID == (int)DocumentType.Clicked)
+                {
+                    count = updatedDocument.ClickCount + 1;
+                    updatedDocument.ClickCount = count;
+                }
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var model = new DocumentStatistics();
+                model.UserId = userID;
+                model.AttachmentId = attachmentID;
+                model.OpenedCount = actionTypeID == (int)DocumentType.Opened ? 1 : 0;
+                model.ClickCount = actionTypeID == (int)DocumentType.Clicked ? 1 : 0;
+                _context.DocumentStatistics.Add(model);
+                await _context.SaveChangesAsync();
+            }
+
+            return count;
+        }
+    }
+}
